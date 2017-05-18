d05834421b59bcb2cee8c18ec19152581b94a3f1
ankewk <171640567@qq.com>
2017-05-19 00:32:36 +0800

    add new composer

---
+++ src/Wxpay/Payment/pay123.php
@@ -0,0 +1,11 @@
+<?php
+namespace Wxpay\Payment;
+
+use EasyWeChat\Payment\Payment;
+
+class pay123 {
+    public static function info()
+    {
+        echo "This is composertest!<br />";
+    }
+}
